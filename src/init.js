5bf114aae02cce091ff4037908950a993b4626b8
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-05-28 18:21:49 +0300

    improve main logic

--- src/init.js
+++ src/init.js
@@ -3,12 +3,39 @@ import 'bootstrap';
 import * as yup from 'yup';
 import onChange from 'on-change';
 import _ from 'lodash';
+import i18n from 'i18next';
+import axios from 'axios';
+import parse from './parser.js';
 import render from './render.js';
+import resources from './locales/index.js';
+
+// const languages = ['en', 'ru'];
 
 export default () => {
+  const defaultLanguage = 'en';
+  const i18nInstance = i18n.createInstance();
+  i18nInstance.init({
+    lng: defaultLanguage,
+    debug: false,
+    resources,
+  });
+
+  yup.setLocale({
+    mixed: {
+      notOneOf: ({ url }) => ({ key: 'feedback.existing_rss', values: { url } }),
+      required: ({ url }) => ({ key: 'feedback.empty_field', values: { url } }),
+    },
+    string: {
+      url: ({ url }) => ({ key: 'feedback.invalid_url', values: { url } }),
+    },
+  });
+
   const elements = {
     formEl: document.querySelector('.rss-form'),
+    inputEl: document.querySelector('#url-input'),
     feedbackEl: document.querySelector('.feedback'),
+    postsContainerEl: document.querySelector('.posts'),
+    feedsContainerEl: document.querySelector('.feeds'),
   };
 
   const initialState = {
@@ -16,17 +43,19 @@ export default () => {
       state: 'filling',
       valid: false,
       fields: {
-        url: [],
+        urls: [],
       },
-      errors: [],
+      errors: '',
     },
     loadingProcess: {
+      state: '',
       posts: [],
       feeds: [],
+      errors: '',
     },
   };
 
-  const watchedState = onChange(initialState, render(elements, initialState));
+  const watchedState = onChange(initialState, render(elements, initialState, i18nInstance));
 
   const validate = (data, urls) => {
     const schema = yup.string().trim().required().url()
@@ -34,34 +63,52 @@ export default () => {
     return schema.validate(data, { abortEarly: false });
   };
 
-  const getData = () => _.noop;
-
-  const updateErrors = (errors, watchedState) => {
-    watchedState.addingFeedsForm = {
-      state: 'filling',
-      valid: false,
-      errors,
-    };
+  const updateErrors = (errors, { addingFeedsProcess }) => {
+    addingFeedsProcess.valid = false;
+    addingFeedsProcess.errors = errors;
+    addingFeedsProcess.state = 'filling';
   };
 
-  const formElement = document.querySelector('.rss-form');
-  formElement.addEventListener('submit', (event) => {
+  const updateData = ({ addingFeedsProcess }, data) => {
+    addingFeedsProcess.state = 'sending';
+    addingFeedsProcess.valid = true;
+    addingFeedsProcess.errors = null;
+    addingFeedsProcess.fields.urls.push(data);
+  };
+
+  const updateFeedback = ({ loadingProcess }, data) => {
+    loadingProcess.posts.unshift(...data.posts);
+    loadingProcess.feeds.unshift(data.feed);
+    loadingProcess.state = 'success';
+  };
+
+  elements.formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = formData.get('url');
     const { urls } = initialState.addingFeedsProcess.fields;
     validate(data, urls)
       .then(() => {
-        watchedState.addingFeedsForm = {
-          state: 'sending',
-          valid: true,
-          errors: null,
-        };
-        getData();
-        // здесь будет выполняться асинхронный запрос на сервер
+        updateData(watchedState, data);
       })
-      .catch(({ errors }) => {
-        updateErrors(errors, watchedState);
+      .catch((err) => {
+        const messages = err.errors.map((e) => i18nInstance.t(e.key)).join('');
+        updateErrors(messages, watchedState);
+      })
+      .then(() => axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(`${data}`)}`))
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return parse(response);
+        }
+        watchedState.loadingProcess.errors = 'Network problem';
+      })
+      .catch((_) => {
+        watchedState.loadingProcess.errors = (`${i18nInstance.t('feedback.loading_failed')}`);
+        watchedState.loadingProcess.state = 'failed';
+        _.noop();
+      })
+      .then((parsedData) => {
+        updateFeedback(watchedState, parsedData);
       });
   });
 };
