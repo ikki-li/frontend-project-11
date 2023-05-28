4535a5cea49bb27a5eac0896defc3f94cfba76ef
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-05-28 18:23:59 +0300

    add lngs for i18next

---
+++ src/locales/en.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    feedback: {
+      empty_field: 'This is a required field',
+      invalid_url: 'This must be a valid URL',
+      existing_rss: 'RSS already exists',
+      success_loading: 'RSS was loaded successfully',
+      loading_failed: 'Resource does not contain valid RSS',
+    },
+    content: {
+      posts: {
+        title: 'Posts',
+        button: 'See more',
+      },
+      feeds: {
+        title: 'Feeds',
+      },
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    feedback: {
+      empty_field: 'Поле должно быть заполнено',
+      invalid_url: 'Ссылка должна быть валидным URL',
+      existing_rss: 'RSS уже существует',
+      success_loading: 'RSS загружен успешно',
+      loading_failed: 'Ресурс не содержит валидный RSS',
+    },
+    content: {
+      posts: {
+        title: 'Посты',
+        button: 'Просмотр',
+      },
+      feeds: {
+        title: 'Фиды',
+      },
+    },
+  },
+};
