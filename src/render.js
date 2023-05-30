cb458dab867ced44d297487ad50b1d85dfdf80d3
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-05-30 15:11:22 +0300

    update state

--- src/render.js
+++ src/render.js
@@ -2,15 +2,15 @@ export default (elements, initialState, i18nInstance) => (path, value, previousV
   const {
     formEl, inputEl, feedbackEl, postsContainerEl, feedsContainerEl,
   } = elements;
-  if (initialState.addingFeedsProcess.valid === false) {
+  if (initialState.form.valid === false) {
     inputEl.classList.remove('is-valid');
     inputEl.classList.add('is-invalid');
     feedbackEl.classList.remove('text-success');
     feedbackEl.classList.add('text-danger');
-    feedbackEl.textContent = initialState.addingFeedsProcess.errors;
+    feedbackEl.textContent = initialState.form.errors;
     return;
   }
-  if (initialState.addingFeedsProcess.valid === true) {
+  if (initialState.form.valid === true) {
     inputEl.classList.remove('is-invalid');
     feedbackEl.classList.remove('text-danger');
     feedbackEl.classList.add('text-success');
@@ -18,8 +18,8 @@ export default (elements, initialState, i18nInstance) => (path, value, previousV
     formEl.reset();
     inputEl.focus();
   }
-  if (initialState.loadingProcess.state === 'success') {
-    const { posts, feeds } = initialState.loadingProcess;
+  if (initialState.processState === 'processed') {
+    const { posts, feeds } = initialState.data;
     postsContainerEl.innerHTML = '';
     feedsContainerEl.innerHTML = '';
     const cardFeedsEl = document.createElement('div');
@@ -29,9 +29,9 @@ export default (elements, initialState, i18nInstance) => (path, value, previousV
     postsContainerEl.append(cardPostsEl);
     cardPostsEl.outerHTML = `<div class="card border-0"><div class="card-body"><h2 class="card-title h4">${i18nInstance.t('content.posts.title')}</h2></div><ul class="list-group border-0 rounded-0">${posts.map((post) => `<li class="list-group-item border-0 border-end-0 d-flex justify-content-between"><a href="${post.link}" class="fw-bold" rel="external">${post.name}</a><button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal">${i18nInstance.t('content.posts.button')}</button></li>`).join('')}</ul></div>`;
   }
-  if (initialState.loadingProcess.state === 'failed') {
+  if (initialState.processState === 'failed') {
     feedbackEl.classList.remove('text-success');
     feedbackEl.classList.add('text-danger');
-    feedbackEl.textContent = initialState.loadingProcess.errors;
+    feedbackEl.textContent = initialState.processErrors;
   }
 };
